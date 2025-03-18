d6d61615fe25729182b1a447785f31fd221f7adb
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2025-03-18 19:35:38 +0530

    Create _layout(out).tsx

---
+++ _layout(out).tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { useFonts, Poppins_400Regular, Poppins_600SemiBold, Poppins_700Bold } from '@expo-google-fonts/poppins';
+
+export default function RootLayout() {
+  useFrameworkReady();
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="+not-found" options={{ presentation: 'modal' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </>
+  );
+}
